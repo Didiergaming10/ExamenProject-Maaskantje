4f4b1f5c9212f6f33144bf9b83180f59416a620c
nicole2a <nicoleabgiri@gmail.com>
2025-05-14 11:30:56 +0200

    php-connectie

---
+++ php/connection.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "mysql";
+$username = "root";
+$password = "password";
+$db = "Eindproject";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+return $conn;
+?>
+
